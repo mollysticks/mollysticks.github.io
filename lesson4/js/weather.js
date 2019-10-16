812fbc6970ac8ab3d7009ed777f02fea839e0199
mollysticks <mollystix@gmail.com>
2019-10-15 21:07:02 -0500

    initial week 4 save

---
+++ lesson4/js/weather.js
@@ -0,0 +1,30 @@
+
+//getting current year
+function displayYear(date) {
+    //set the year from the date object to a new variable
+    let n = date.getFullYear();
+    //put the year right before the copyright symbol
+    document.getElementById("currentYear").innerHTML = n;
+}
+
+function displayCurrentDate(date) {
+    //set month and day names
+    let monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+	let weekNames = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    let day = weekNames[date.getDay()];
+    let month = monthNames[date.getMonth()];
+    
+    let today = day + ', ' + date.getDate() + ' ' + month + ' ' + date.getFullYear();
+    document.getElementById("currentDate").innerHTML = today;
+    
+}
+
+function displayDates() {
+    let date = new Date;
+    displayYear(date);
+    displayCurrentDate(date);
+}
+
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
