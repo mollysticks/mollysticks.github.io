fdb3c0e2c34dfa26a2ee8b01b139476096ea725b
mollysticks <mollystix@gmail.com>
2019-10-26 19:44:39 -0500

    windchill calculations fixed

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,9 +1,16 @@
 function computeWindchill() {
-    console.log("check");
     let f = parseFloat(document.getElementById("high-temp").textContent);
     let w = parseFloat(document.getElementById("wind-speed").textContent);
-    console.log(f);
-    console.log("This file is working");
+    let windSpeed = Math.pow(w, 0.16);
+    let firstWindChill = (35.74 + (0.6215 * f) - (35.75 * windSpeed) + 0.4275 * f * windSpeed);
+    let windChill = Math.round(firstWindChill);
+
+    if (f > 50 || w < 3) {
+        document.getElementById("chill").innerHTML = "N/A"; 
+    }
+    else {
+        document.getElementById("chill").innerHTML = windChill + "&#8457;";
+    }
 }
 
 document.onload = computeWindchill();
