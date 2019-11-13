fee9e25fe38ff493b6b05ce151ee82829100de59
mollysticks <mollystix@gmail.com>
2019-11-12 22:46:48 -0600

    storm center report form push

---
+++ lesson-8/js/weather.js
@@ -0,0 +1,44 @@
+
+//getting current year
+function displayYear(date) {
+    //set the year from the date object to a new variable
+    let n = date.getFullYear();
+    //put the year right before the copyright symbol
+    document.getElementById("currentYear").innerHTML = n;
+}
+
+function displayCurrentDate(date) {
+    //set month and day names
+    let monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+	let weekNames = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    let day = weekNames[date.getDay()];
+    let month = monthNames[date.getMonth()];
+    
+    let today = day + ', ' + date.getDate() + ' ' + month + ' ' + date.getFullYear();
+    document.getElementById("currentDate").innerHTML = today;
+    
+}
+
+function displayDates() {
+    let date = new Date;
+    displayYear(date);
+    displayCurrentDate(date);
+    toggleFriday(date);
+}
+
+function toggleFriday(date) {
+    let day = date.getDay();
+    if (day == 5) {
+        document.getElementById("sat-banner").style.display = "block";
+    }
+}
+
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+//adjust rating function for storm center form page
+function adjustRating(rating) {
+    document.getElementById("severity").innerHTML = rating;
+}
+
---
+++ preston-page/js/lazyload.js
@@ -0,0 +1,35 @@
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if(!src) {
+        return;
+    }
+    img.src = src;
+}
+
+const images = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+    threshhold: 1,
+    rootMargin: "0px 0px 300px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {image.removeAttribute('data-src');};
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {        
+    if (!entry.isIntersecting) {
+        return;
+    }        
+    else {
+        preloadImage(entry.target);
+        imgObserver.unobserve(entry.target);
+    }
+});
+}, imgOptions);
+
+images.forEach(image => {
+    imgObserver.observe(image);
+});
